01fb9f78c8bec3503d67190d0a6c59391fb9d390
Ramay03 <ramaymaheshwari@gmail.com>
2022-07-09 03:17:38 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,47 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "1MB is equal to",
+    answer: "1024KB",
+    options: [
+      "1024B",
+      "1024KB",
+      "1024GB",
+      "None"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which function do we use for checking if a character is a space or a tab?",
+    answer: "isblank()",
+    options: [
+      "isdigit()",
+      "isalnum()",
+      "isblank()",
+      "isalpha()"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Best time complexity of insertion sort",
+    answer: "O(n)",
+    options: [
+      "O(n)",
+      "O(nlogn)",
+      "O(1)",
+      "O(logn)"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+];
